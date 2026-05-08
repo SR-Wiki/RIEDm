ca6030af114cee75cff4c7762be8dbff7cbbd356
jiahuiGUI <62888978+jiahuiGUI@users.noreply.github.com>
2026-05-08 23:06:20 +0800

    Delete demo.m

--- demo.m
+++
@@ -1,9 +0,0 @@
-% clear;clc;close all;
-%% Involve RIED
-addpath(genpath('./RIED_core'));
-addpath(genpath('./Utils'));
-%% RIED recon
-imgstack = imreadstack('RIEDm_data/bl.tif');
-RIEDrecon = RIEDm(imgstack,'pixel',160,'NA',1.42,'wavelength',517,'iter1',4,'subfactor',0,'fidelity',150,'sparsity',20,'iter2',5);
-%% Visualization
-visualize(imgstack, RIEDrecon, 560, 1, [1, 99.98], 'Yellowhot');
